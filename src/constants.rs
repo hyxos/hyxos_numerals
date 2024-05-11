dc681cec9539d0299553d37b52a64a5456541217
Donnie <donnie@hyxos.io>
2024-05-10 23:38:17 -0400

    initial commit

---
+++ src/constants.rs
@@ -0,0 +1,19 @@
+pub mod constants {
+  pub const DIACRITIC_CHARS: [char;5] = ['t', 's', 'r', 'j', 'w'];
+  pub const DIACRITIC_NAME: [&str;5] = ["ta", "she", "ri", "jo", "wu"];
+  pub const DUODECIMALS: [char;12] = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B'];
+  pub const DUODECIMAL_NAME: [&str;12] = ["zo", "zee", "bey", "tree", "kat", "pen", "hyx", "sep", "awk", "nen", "dek", "lev"];
+  pub const COLORS: [&str;5] = ["yellow", "blue", "red", "purple", "green"];
+  pub const PLANETS: [&str;5] = ["Saturn", "Mercury", "Mars", "Venus", "Jupiter"];
+  pub const ELEMENTS: [&str;5] = ["earth", "water", "fire", "metal", "wood"];
+  pub const GENERATING_INDECES: [u8;5] = [2, 4, 1, 3, 0];
+  pub const ANIMALS: [&str;12] = ["pig", "rat", "ox", "tiger", "rabbit", "dragon", "snake", "horse", "sheep", "monkey", "rooster", "dog"];
+  pub const WESTERN_SIGNS: [&str;12] = ["pisces", "aries", "taurus", "gemini", "cancer", "leo", "virgo", "libra", "scorpio", "sagitarius", "capricorn", "aquarius"];
+  pub const ELEMENTS_CHINESE: [&str;5] = ["木", "火", "土", "金", "水"];
+  pub const EARTHLY_BRANCHES_CHINESE: [&str;12] = ["亥", "子", "丑" ,"寅", "卯", "辰", "巳", "午", "未", "申", "酉", "戌"];
+  pub const HEAVENLY_STEMS_CHINESE: [&str;10]= ["甲","乙","丙","丁","戊","己","庚","辛","壬","癸"];
+  pub const ANIMALS_CHINESE: [&str;12] = ["猪", "鼠", "牛", "虎", "兔", "龍", "蛇", "馬", "羊", "猴", "雞", "狗"];
+  pub const POLARITY: [&str;2] = ["yin", "yang"];
+  pub const POLARITY_CHINESE: [&str;2] = ["陰", "陽"];
+  pub const POLARITY_LUMINARIES: [&str;2] = ["Moon", "Sun"];
+}
